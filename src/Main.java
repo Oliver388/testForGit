add9ea78aed9fb1f7dc4cb1805ef2ae7f1cf278b
Oliver388 <oliverling12345@gmail.com>
2022-09-23 16:08:54 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author linglifan
+ * @date ${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
